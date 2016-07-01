e1c27504c25f122dd4f0232ac6cf7a48e3b86666
Leandro Lourenci <leandrolourenci@gmail.com>
2016-07-01 16:41:52 -0300

    fix to imagga_scale

--- CloudinarySwift/Source/Crop.swift
+++ CloudinarySwift/Source/Crop.swift
@@ -8,20 +8,41 @@
 
 import Foundation
 
-public enum Crop: String, TransformationType {
-    case scale, fit, limit, mfit, fill, lfill, pad, lpad, mpad, crop, thumb, imaggaCrop, imaggaScale
+public enum Crop: TransformationType {
+    case scale, fit, limit, mfit, fill, lfill, pad, lpad, mpad, crop, thumb, imaggaCrop, imaggaScale(Height, Width)
 
     public var paramOfURL: String {
         return "c"
     }
     
     public var valueOfParam: String {
-        if self == .imaggaCrop {
+        switch self {
+        case .scale:
+            return "scale"
+        case .fit:
+            return "fit"
+        case .limit:
+            return "limit"
+        case .mfit:
+            return "mfit"
+        case .fill:
+            return "fill"
+        case .lfill:
+            return "lfill"
+        case .pad:
+            return "pad"
+        case .lpad:
+            return "lpad"
+        case .mpad:
+            return "mpad"
+        case .crop:
+            return "crop"
+        case .thumb:
+            return "thumb"
+        case .imaggaCrop:
             return "imagga_crop"            
-        } else if self == .imaggaScale {
-            return "imagga_scale"
-        } else {
-            return self.rawValue
+        case let .imaggaScale(h, w):
+            return "imagga_scale,\(h.URL),\(w.URL)"
         }
     }
 }
