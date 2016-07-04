1e26e235b8c10b76e2a786f863f698983ceb163c
Leandro Lourenci <leandrolourenci@gmail.com>
2016-07-04 18:04:43 -0300

    Added test for Uploader

---
+++ Tests/Uploader.swift
@@ -0,0 +1,26 @@
+//
+//  Uploader.swift
+//  CloudinarySwift
+//
+
+import XCTest
+@testable import CloudinarySwift
+
+class Uploader: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+
+    }
+    
+}
